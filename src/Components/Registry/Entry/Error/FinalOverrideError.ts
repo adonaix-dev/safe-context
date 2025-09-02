9086c68e74498af5ab9a1d01a29696ceb5ce797b
Arthur Adonai <tutuadonai@gmail.com>
2025-09-02 00:28:30 -0300

    fix: update error message and name in FinalOverrideError for clarity and consistency

--- src/Components/Registry/Entry/Error/FinalOverrideError.ts
+++ src/Components/Registry/Entry/Error/FinalOverrideError.ts
@@ -2,13 +2,15 @@ import { SafeContextErrorCode } from "~/Error/Code/SafeContextErrorCode";
 import { SafeContextError } from "~/Error/SafeContextError";
 
 class FinalOverrideError extends SafeContextError {
+    override name = "FinalOverrideError";
+
     constructor() {
         super(SafeContextErrorCode.FinalOverride);
     }
 
     override formatWithKey(key: string): this {
         return (
-            (this.message = `cannot override context with key ${key}. This context is marked as final, and final contexts cannot be overriden`),
+            (this.message = `cannot set context with key ${key}. This context is set and marked as final, and final contexts cannot be overriden`),
             this
         );
     }
