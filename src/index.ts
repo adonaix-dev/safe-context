c8c9157eaf2699661dd47d3e5317bf2a8e89aa80
Arthur Adonai <tutuadonai@gmail.com>
2025-09-02 16:46:11 -0300

    feat(api): expose types and errors in public API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
+export type { DisposableContext } from "~/Disposable/DisposableContext";
+export type { DisposableMulticontext } from "~/Disposable/DisposableMulticontext";
+
+export type { ContextDictionary } from "~/Types/ContextDictionary";
+
+export type { ContextGetOptions } from "~/Types/Options/ContextGetOptions";
+export type { ContextSetOptions } from "~/Types/Options/ContextSetOptions";
+export type { MulticontextGetOptions } from "~/Types/Options/MulticontextGetOptions";
+export type { MulticontextSetOptions } from "~/Types/Options/MulticontextSetOptions";
+
+export type { ContextGetReturn } from "~/Types/Return/ContextGetReturn";
+export type { MulticontextGetReturn } from "~/Types/Return/MulticontextGetReturn";
+export type { MulticontextSetReturn } from "~/Types/Return/MulticontextSetReturn";
+
+export { SuppressedError } from "~/Disposable/Stack/Error/SuppressedError";
+
+export { SafeContextError } from "~/Error/SafeContextError";
+export { SafeContextErrorCode } from "~/Error/Code/SafeContextErrorCode";
+
+export { FinalOverrideError } from "~/Registry/Entry/Error/FinalOverrideError";
+
 export { SafeContext } from "./SafeContext";
