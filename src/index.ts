621dbb805aa2078ae57e207e5d1233c634d09c8d
Arthur Adonai <tutuadonai@gmail.com>
2025-09-24 15:10:42 -0300

    refactor(api): adjust public type exports

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
-export type { DisposableContext } from "~/Disposable/DisposableContext";
-export type { DisposableMultipleContext } from "~/Disposable/DisposableMultipleContext";
-
 export type { ContextDictionary } from "~/Types/ContextDictionary";
 
 export type { GetContextOptions } from "~/Types/Get/GetContextOptions";
 export type { GetMultipleContextOptions } from "~/Types/Get/GetMultipleContextOptions";
+export type { GetMultipleContextReturn } from "~/Types/Get/GetMultipleContextReturn";
 
 export type { SetContextOptions } from "~/Types/Set/SetContextOptions";
+export type { SetContextReturn } from "~/Types/Set/SetContextReturn";
 export type { SetMultipleContextOptions } from "~/Types/Set/SetMultipleContextOptions";
+export type { SetMultipleContextReturn } from "~/Types/Set/SetMultipleContextReturn";
 
+export type { WithContextChanged } from "~/Types/With/WithContextChanged";
 export type { WithContextOptions } from "~/Types/With/WithContextOptions";
 export type { WithMultipleContextOptions } from "~/Types/With/WithMultipleContextOptions";
+export type { WithMultipleContextScope } from "~/Types/With/WithMultipleContextScope";
 
-export { SuppressedError } from "~/Disposable/Stack/Error/SuppressedError";
+export type { DisposableContext } from "~/Disposable/DisposableContext";
+export type { DisposableMultipleContext } from "~/Disposable/DisposableMultipleContext";
 
 export { SafeContextError } from "~/Error/SafeContextError";
-export { SafeContextErrorCode } from "~/Error/SafeContextErrorCode";
 
 export { FinalOverrideError } from "~/Registry/Entry/Error/FinalOverrideError";
 
