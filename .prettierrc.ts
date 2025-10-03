b052d20732bd5f584c8745eea4b0ba11301ca03c
Arthur Adonai <tutuadonai@gmail.com>
2025-10-03 03:30:05 -0300

    refactor(prettierrc) do random thing in prettierrc

--- .prettierrc.ts
+++ .prettierrc.ts
@@ -1,3 +1,4 @@
-import prettierrc, { type PrettierConfig } from "adonaix-lib/.prettierrc.ts";
+import prettierrc from "adonaix-lib/.prettierrc.ts";
+import type { PrettierConfig } from "adonaix-lib/.prettierrc.ts";
 
 export default { ...prettierrc } satisfies PrettierConfig;
